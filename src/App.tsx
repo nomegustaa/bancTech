08a41a98ef350d6ddbf3b16cc79c852b144c04e1
Gustavo Carvalho da Silva <gustavo.silva@nm.local>
2023-10-26 14:52:44 -0300

    fix: setting provider theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import AppRoutes from './routes/AppRoutes';
 import { SalesProvider } from './context/SalesContext';
 import { GlobalStyle } from './styles';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
 
 const App = () => {
   return (
-    <SalesProvider>
-      <GlobalStyle />
-      <AppRoutes />
-    </SalesProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <SalesProvider>
+        <GlobalStyle />
+        <AppRoutes />
+      </SalesProvider>
+    </ThemeProvider>
   );
 };
 
